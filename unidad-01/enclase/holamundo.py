8ed93e839a63e8597298881f060e8a73a995b4a9
Juampa <juanpablososa@gmail.com>
2023-08-17 16:39:54 -0300

    ejercicios unidad 02

---
+++ unidad-01/enclase/holamundo.py
@@ -0,0 +1,33 @@
+
+
+
+# Acá lo que voy a hacer es escribir en pantalla.....
+
+# esta linea no es tomada por Python.......
+
+# 
+# Acá escribo en pantalla
+
+
+result_uno = 10 + 3 * 5 + 2 * 2
+
+result_dos = result_uno * 100
+
+print(result_dos)  # imprime en pantalla el valor de la variable_uno
+
+
+
+assert = "Juan"
+
+print(assert)
+
+
+
+# sumale al resultado
+
+
+
+
+
+
+
---
+++ unidad-02/ejercicios/practicar.py
@@ -0,0 +1,59 @@
+
+
+
+
+
+
+
+# Ejercicio 1
+# Crear una variable, asignarle el valor 5
+# Crear otra variable, asignarle el valor 10
+# Crear una tercera variable, asignarle la suma de las dos variables anteriores
+# Convertir la tercera variable a string, asignarle otro nombre de variable
+# Crear una variable resultado, que concatene el texto "El resultado es: " con la variable anterior, e imprimir en pantalla
+
+
+
+# Ejercicio 2
+# Crear una variable int, que contenga la operación de hacer tres al cuadrado, sumar 10 y multiplicar por 2
+# Crear una variable float, que contenga la operación de dividir 10 entre 3
+# Crear una variable string, que contenga el texto "El resultado es: " y la variable anterior
+
+# Ejercicio 3
+# Crear una variable en int, aignarle un valor cualquiera, y luego conevrtirla a float en otra variable, y luego a string.
+# Imprimir en panatalla el tipo de dato de cada variable, su nombre y el valor de cada variable.
+# Ejemplo para imprimir en pantalla -> La variable llamada "var_int" es de tipo <class 'int'> y su valor es 5
+
+# Ejercicio 4
+# Crear una variable que haga un culculo aritmetico que contenga todas las operaciones
+# Mostrar el resultado en pantalla
+# Y Mostrar en pantalla la explicación de porque dió ese resultado, es decir como fue elijiendo las prioridades Python para hacer las operaciones
+
+# Ejercicio 5
+# Crear una variable que contenga el texto Hola Mundo, y luego mostrar en pantalla el texto "Hola" y luego el texto "Mundo" en dos lineas diferentes
+# Luego imprimir en pantalla el Hola Mundo pero que se muestre con comillas simples, es decir 'Hola Mundo'
+
+# Ejercicio 6
+# Imrpmir en pantalla un cuadrado de 5x10 con el caracter #, es decir:
+# ##########
+# ##########
+# ##########
+# ##########
+# ##########
+# Para hacer ésto untilizar en string el caracter \n, que es el caracter de salto de linea, y también el caracter * que sirve para repetir un caracter n veces, por ejemplo:
+# print("Juan" * 5) -> imprime en pantalla
+
+# Ejercicio 7
+# Escribir un string, luego immprimirlo en pantalla que utilice al menos 3 secuencias de escape de Python, están en la página 20 del libro de estudio.
+
+# Ejercicio 8
+# Escribir en pantalla, 5 nombres de variables que sean correctos, uno debajo del otro, y luego 5 nombres de variables que sean incorrectos, uno debajo del otro, y explicar porque son incorrectos.
+
+# Ejercicio 9
+# Leer final de la Unidad 2 del manual, explica la función input()
+# Crear un programa que lea el nombre de una persona, y luego imprima en pantalla el texto "Hola" y el nombre de la persona.
+
+# Ejercicio 10
+# Leer final de la Unidad 2 del manual, explica la función input()
+# Crear un programa que pregunte por el nombre de una persona, luego el apellido y la edad.
+# Que imprime en pantalla -> El nombre es: xxxx, su apellido es:xxxx y tiene xx años.
