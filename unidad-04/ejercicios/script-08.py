d03507d082375f7477194ed789d4a63783e11886
Juampa <juanpablososa@gmail.com>
2023-08-24 18:47:26 -0300

    Ejercicios para resolver, Unidad 3 y 4

---
+++ unidad-04/ejercicios/script-08.py
@@ -0,0 +1,10 @@
+
+
+"""
+Escriba un programa que pida una fecha y diga si ese día existe.
+
+El programa no tiene por qué comprobar que se escribe una fecha posterior al 15 de octubre de 1582, que fue cuando se instauró el calendario gregoriano.
+
+Se recuerda que enero, marzo, mayo, julio, agosto, octubre y diciembre tienen 31 días, que abril, junio, septiembre y noviembre tienen 30 días y febrero tiene 28 días salvo los años bisiestos en que tiene 29.
+
+"""
---
+++ unidad-04/ejercicios/script-09.py
@@ -0,0 +1,6 @@
+
+
+"""
+Escriba un programa que pida tres números y diga si el tercero está más cerca del primero o del segundo.
+Notas Haga este ejercicio sin utilizar la función valor absoluto abs(), pero utilizando el ejercicio anterior.
+"""
